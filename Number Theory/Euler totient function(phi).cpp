f1067d5cd93129f86ed7147186f900cbe0446d62
AKASH-ALAM <mesbahulalamakas2014@gmail.com>
2022-12-20 04:12:33 +0600

    phi function

---
+++ Number Theory/Euler totient function(phi).cpp
@@ -0,0 +1,31 @@
+
+const int mx = 32000; // is sqrt of : 1e9
+bitset <mx> isPrime;
+vector <int> prime;
+
+void seive() {
+	isPrime.set();
+	prime.push_back(2);
+	for (int i = 3; i <= mx; i += 2) {
+		if (isPrime[i]) {
+			prime.push_back(i);
+			for (int j = i * i; j <= mx; j += i * 2) isPrime[j] = false;
+		}
+	}
+}
+
+
+int phi(int n) {
+	int result = n;
+	for (int i = 0; prime[i] * prime[i] <= n; i++) {
+		if (n % prime[i] == 0) {
+			while (n % prime[i] == 0) n /= prime[i];
+			result -= result / prime[i];
+		}
+	}
+	if (n > 1) {
+		result -= result / n;
+	}
+
+	return result;
+}
