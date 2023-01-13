35e5b0f2c363c591581e53b0c235680e4f40af13
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2023-01-14 01:30:20 +0600

    update

--- Recursion/n fibonacci.cpp
+++ Recursion/n fibonacci.cpp
@@ -1,40 +1,16 @@
-/**
- *    Author:  AKASH-ALAM       
-**/
 #include <bits/stdc++.h>
-#define endl        '\n'
-#define db          double
-#define ld          long double
-#define ll          long long
-#define ull         unsigned long long
-#define sqr(x)      (x) * (x)
-#define gcd(a, b)   __gcd(a, b)
-#define lcm(a, b)   ((a/gcd(a,b)) * b)
-#define pf(x) 		push_front(x)
-#define pb(x)       push_back(x)
-#define eb(x)		emplace_back(x)
-#define all(x)      (x).begin(), (x).end()
-#define rall(x) 	(x).rbegin(), (x).rend()
-#define sz(x) 		(int)x.size()
-#define debug(x) 	cerr << #x << " = " << (x) <<endl
-#define READ(filename)  freopen(filename, "r", stdin);
-#define WRITE(filename)  freopen(filename, "w", stdout);
-#define Fast_io     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-
-const ld PI = acos((ld)-1);
-const int MOD = 1e9+7;
-const ll INF = 1e18; 
 using namespace std;
 
-int fib(int n) {
-    if(n <= 2) return 1;
-    return fib(n-1) + fib(n-2);
+vector <int> a(100, -1);
+
+int f(int n){
+    if(n <= 1) return a[n] = n;
+    if(a[n-1] == -1) a[n-1] = f(n-1);
+    if(a[n-2] == -1) a[n-2] = f(n-2);
+    return a[n] = a[n-1] + a[n-2];
 }
-
-
 int main(){
-    Fast_io;
-    int n;  cin >> n;
-    cout << fib(n) << endl;
+    cout << f(10) << endl;
+    for(int i = 1; i <= 10; i++) cout << a[i] << " \n"[i == 10];
     return 0;
 }
