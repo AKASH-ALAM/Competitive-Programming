80e18ee402f9976d01b7d72f83691bd62a3bd42d
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2023-03-10 00:52:55 +0600

    Update

--- Algorithm/2sum.cpp
+++ Algorithm/2sum.cpp
@@ -1,89 +1,15 @@
-/**
- *  Author  : Pnictogen
- *  Task    :
- *  Algo    :
-**/
-#include <bits/stdc++.h>
-
-#define endl          '\n'
-#define sqr(x)        (x) * (x)
-#define gcd(x,y)      __gcd(x,y)
-#define lcm(x,y)      ((x/gcd(x,y)) * y)
-#define sz(x)         (int)x.size()
-#define all(x)        (x).begin(),(x).end()
-#define rall(x)       (x).rbegin(),(x).rend()
-#define prec(x)       fixed<<setprecision(x)
-#define min3(a,b,c)   min(a,min(b,c))
-#define max3(a,b,c)   max(a,max(b,c))
-#define min4(a,b,c,d) min(a,min(b,min(c,d)))
-#define max4(a,b,c,d) max(a,max(b,max(c,d)))
-#define unsyncIO      ios_base::sync_with_stdio(false); cin.tie(nullptr)
-
-using namespace std;
-using ll = long long;
-using db = double;
-using ld = long double;
-using ull = unsigned long long;
-
-const ld PI = acos((ld) - 1);
-const int MOD = 1e9 + 7;
-const ll INF = 2e18 + 1;
-const ld EPS = 1e-9;
-const int MX = 2e5;
-
-#ifdef LOCAL
-#include"debug.h"
-#else
-#define debug(...)
-#endif
-
-ll containerSize(const vector <ll>& v, m, mid) {
-	ll cnt = 0, deap = 0;
-	for (int i = 0; i < sz(v); i++) {
-		if (deap <= mid) deap += v[i];
-		if (deap > mid) {
-			deap = 0;
-			cnt++;
-			i--;
-		}
-	}
-	if (cnt < m) cnt = 1;
-	else cnt = 0;
-	return cnt;
-}
 
 void solve() {
-	ll n, m;	cin >> n >> m;
-	vector <ll> v(n);
-	for (int i = 0; i < n; i++) cin >> v[i];
-	ll low = 1, high = 10, mid, ans;
+	int n, target;   cin >> n >> target;
+	int a[n];   for (int i = 0; i < n; i++) cin >> a[i];
 
-	while (low <= high) {
-		mid = low + ((high - low) / 2);
-		ans = containerSize(v, m, mid);
-		if (ans) high = mid - 1;
-		else low = mid + 1;
-	}
-	cout << mid << endl;
-}
-
-int main() {
-
-#ifdef LOCAL
-	clock_t tStart = clock();
-	freopen("in.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
-#endif
-	unsyncIO;
-
-	int t = 1;	cin >> t;
-
-	while (t--) {
-		solve();
+	int cnt = 0;
+	map <int, int> freq;
+	for (int i = 0; i < n; i++) {
+		int need =  target - a[i];
+		cnt += freq[need];
+		freq[a[i]]++;
 	}
 
-#ifdef LOCAL
-	cerr << "\nRuntime: " << (ld) (clock() - tStart) / CLOCKS_PER_SEC << " Seconds" << endl;
-#endif
-	return 0;
+	cout << cnt << endl;
 }
--- Algorithm/nextGreaterElement.cpp
+++ Algorithm/nextGreaterElement.cpp
@@ -1,30 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-const int N = 100005;
-int arry[N], ans[N];
-
-int main() {
+void solve() {
 	int n;	cin >> n;
-	for (int i = 1; i <= n; i++)	cin >> arry[i];
-
+	int a[n + 1], ans[n]; for (int i = 1; i <= n; i++) cin >> a[i];
 	stack <int> s;
 
-	for (int i = n; i >= 0; i--) { // we iterate from back
-		while (!s.empty() and s.top() <= arry[i]) {
-			s.pop();
-		}
-		if (s.empty()) {
-			ans[i] = -1;
-		} else {
-			ans[i] = s.top();
-		}
-
-		s.push(arry[i]);
+	for (int i = n; i >= 0; i--) { // we iterate from right to left
+		while (!s.empty() and s.top() <= a[i]) s.pop();
+		if (s.empty()) ans[i] = -1;
+		else ans[i] = s.top();
+		s.push(a[i]);
 	}
 
-	for (int i = 1; i <= n; i++) cout << ans[i] << " ";
-
-	cout << endl;
-	return 0;
+	for (int i = 1; i <= n; i++) cout << ans[i] << " \n"[i == n];
 }
