0efa9a9502ca2bdf119cece92869c49fd078802b
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2023-08-17 00:59:05 +0600

    update

--- STL/priority_queue.cpp
+++ STL/priority_queue.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){ 
+class Compare {
+public:
+    bool operator()(pair <int, int> a, pair <int, int> b) {
+        if (a.first < b.first) return true;
+        else if (a.first == b.first and a.second > b.second) return true;
+        return false;
+    }
+};
+
+
+int main() {
+    priority_queue <pair<int, int>, vector <pair<int, int>>,  Compare> pq;
+
     priority_queue <string> q;
     q.push("Alam");
     q.push("Pakhi");
@@ -10,7 +22,7 @@ int main(){
     q.push("Rasel");
     q.push("Akash");
 
-    while(!q.empty()){
+    while (!q.empty()) {
         string s = q.top();
         cout << s << endl;
         q.pop();
@@ -25,11 +37,11 @@ int main(){
     pq.pop(); // 10
     pq.pop(); // 4
 
-    while(!pq.empty()) {
+    while (!pq.empty()) {
         cout << pq.top() << endl;
         pq.pop();
     }
-            cout << endl;
+    cout << endl;
     priority_queue <int, vector <int>, greater <int>> pv;
     pv.push(2);
     pv.push(1);
@@ -37,7 +49,7 @@ int main(){
     pv.push(0);
     pv.push(5);
 
-    while(!pv.empty()) {
+    while (!pv.empty()) {
         cout << pv.top() << endl;
         pv.pop();
     }
