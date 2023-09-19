899da9e13434432b39e43d0b15def665c65c45d9
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2023-09-20 03:07:34 +0600

    init

--- Bits Manipulation/subset.cpp
+++ Bits Manipulation/subset.cpp
@@ -1,28 +1,12 @@
-/**
- *  Author  : Pnictogen
- *  Date    : 2022-11-28 22:48:01
- *  Task    : subset
-**/
-#include <bits/stdc++.h>
-using namespace std;
-
-bool check(int x, int i) {
-    return (x & (1 << i));
-}
-
-void solve(){
-    vector <int> v = { 1, 2, 3 };
-    int size = 1 << v.size();
-    for (int mask = 0; mask < size; mask++) {
-        for (int i = 0; i < 3; i++) {
-            if (check(mask, i)) {
+//O(2^n)
+void subset() {
+    vector <int> v = { 1, 2, 3};
+    for (int mask = 0; mask < (1 << v.size()); mask++) {
+        for (int i = 0; i < v.size(); i++) {
+            if (mask & (1 << i)) {
                 cout << v[i] << ' ';
             }
-        }cout << endl;
+        } cout << endl;
     }
 }
 
-int main() {
-    solve();
-    return 0;
-}
