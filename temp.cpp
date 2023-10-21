b27357ca55eb72b683bb9ed617e5fc7bc64ab724
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2023-10-21 23:48:00 +0600

    update

--- temp.cpp
+++ temp.cpp
@@ -18,7 +18,7 @@ using ull = unsigned long long;
 #define testcase      cout << "Case " << cs++ << ":"
 string  tostr(int n)  {stringstream rr; rr << n; return rr.str();}
 #define unsyncIO      ios_base::sync_with_stdio(false); cin.tie(nullptr)
-//stol(s);  x.to_string();
+//stol(s);  to_string(x);
 const ld PI = acos((ld) - 1);
 const int MOD = 1e9 + 7;
 const ll INF = 2e18 + 1;
@@ -32,33 +32,74 @@ int  cs = 1;
 #define debug(...)
 #endif
 
-bool detect(int n, vector <int> *g, int src) {
-	queue <pair<int , int>> q; // node, parent
-	vector <bool> vis(n + 1, 0);
-	q.push({src, -1});
-	vis[src] = 1;
-	while (!q.empty()) {
-		auto [node, parent] = q.front();
-		q.pop();
-		for (auto adjnode : g[node]) {
-			if (!vis[adjnode]) {
-				vis[adjnode] = 1;
-				q.push({adjnode, node});
-			} else if (parent != adjnode) return true;
-		}
+int dx[] = { -1, 1, 0, 0, -1, -1, 1, 1};
+int dy[] = {  0, 0, -1, 1, -1, 1, -1, 1};
+
+
+string DecimalToBinary(ll num) {
+	string str;
+	while (num) {
+		if (num & 1LL) str += '1';
+		else str += '0';
+		num >>= 1LL; // Right Shift by 1
 	}
-	return false;
+	return str;
 }
+
 void solve() {
-	int n, m;  cin >> n >> m;
-	vector <int> g[n + 1];
-	for (int i = 0; i < m; i++) {
-		int u, v;   cin >> u >> v;
-		g[u].push_back(v);
+	ll n;	cin >> n;
+	vector <string> grid;
+	ll limit = 0;
+
+	for (int i = 0; i < n ; i++) {
+		ll x;
+		cin >> x;
+		string s = DecimalToBinary(x);
+		limit = max(limit, (ll) sz(s));
+		grid.push_back(s);
+	}
+	if (limit == 0) {
+		cout << "Yeeee! ice-cream." << endl;
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		while (sz(grid[i]) < limit) grid[i].push_back('0');
+		reverse(all(grid[i]));
 	}
 
-	if (detect(n, g, 1)) cout << "Yes" << endl;
-	else cout << "No" << endl;
+	// for (auto it : grid) cout << it << endl;
+
+	int m = limit;
+	queue <pair<int, int>> q;
+	q.push({0, 0});
+	vector <vector<bool>> vis(n, vector <bool> (m, 0));
+	vis[0][0] = 1;
+	char init = grid[0][0];
+	bool ans = 0;
+
+	while (!q.empty()) {
+		auto [r, c] = q.front();
+		q.pop();
+		if (r == n - 1 and c == m - 1) {
+			ans = 1;
+			break;
+		}
+		for (int i = 0; i < 4; i++) {
+			int nrow = r + dx[i];
+			int ncol = c + dy[i];
+			if (nrow >= 0 and ncol >= 0 and nrow < n and ncol < m and !vis[nrow][ncol] and init == grid[nrow][ncol]) {
+				vis[nrow][ncol] = 1;
+				q.push({nrow, ncol});
+			}
+		}
+	}
+
+	if (ans) {
+		cout << "Yeeee! ice-cream." << endl;
+	} else {
+		cout << "Let's make ice-cream." << endl;
+	}
+
 }
 
 int main() {
