1f9125bef5b27f59be7aefaa4ce4ab26f2fc38a8
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2023-12-09 11:42:58 +0600

    add time complexity

--- String/LCS using DP.cpp
+++ String/LCS using DP.cpp
@@ -6,36 +6,29 @@ int LCS(int i, int j, string &a, string &b, vector <vector<int>> &dp) {
 	return dp[i][j] = max(LCS(i - 1, j, a, b, dp), LCS(i, j - 1, a, b, dp));
 }
 
-//Printing lcs
-int lcs(int i, int j, string &a, string &b, vector <vector<int>> &dp) {
-	if (i < 0 or j < 0) return 0;
-	if (dp[i + 1][j + 1] != -1) return dp[i + 1][j + 1];
-	if (a[i] == b[j]) return dp[i + 1][j + 1] = 1 + lcs(i - 1, j - 1, a, b, dp);
-	return dp[i + 1][j + 1] = max(lcs(i - 1, j, a, b, dp), lcs(i, j - 1, a, b, dp));
-}
-
-void solve() {
-	string a, b;
-	cin >> a >> b;
-
+//Printing lcs using tabulation method
+string lcs(string a, string b) {
 	int n = sz(a), m = sz(b);
-	vector <vector<int>> dp(n + 1, vector <int> (m + 1, -1)); // 1->based indexing
+	vector <vector<int>> dp(n + 1, vector <int> (m + 1, 0)); // 1->based indexing
 
-	int cnt = lcs(n - 1, m - 1, a, b, dp);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (a[i - 1] == b[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+			else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
 
 	string ans = "";
-	for (int i = 0; i < cnt; i++) ans += '$';
-	int i = n, j = m, idx = cnt - 1;
-
+	int i = n, j = m, len = dp[n][m], idx = len - 1;
+	for (int i = 0; i < len; i++) ans += '$';
+	//O(n + m) at the worst case
 	while (i > 0 and j > 0) {
 		if (a[i - 1] == b[j - 1]) {
-			ans[idx] = a[i - 1];
-			idx--;
+			ans[idx--] = a[i - 1];
 			i--, j--;
 		}
 		else if (dp[i - 1][j] > dp[i][j - 1]) i--;
 		else j--;
 	}
-
-	cout << ans << endl;
+	return ans;
 }
