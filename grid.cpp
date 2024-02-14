412eff1ba4869c0b06bd477e8b9b395fb08c3734
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2024-02-15 01:57:36 +0600

    update

--- grid.cpp
+++ grid.cpp
@@ -1,30 +1,17 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
-using namespace __gnu_pbds;
-using ll = long long;
 using ld = long double;
+const ld PI = acos((ld) - 1);
 using ull = unsigned long long;
 
 #define endl          '\n'
-#define gcd(x,y)      __gcd(x,y)
+#define int           long long
 #define sz(x)         (int)x.size()
-#define lcm(x,y)      ((x/gcd(x,y)) * y)
 #define mem(a,x)      memset(a,x,sizeof(a))
 #define all(x)        (x).begin(),(x).end()
 #define rall(x)       (x).rbegin(),(x).rend()
-#define prec(x)       fixed << setprecision(x)
-#define testcase      cout << "Case " << cs++ << ":"
-#define unsyncIO      ios_base::sync_with_stdio(false); cin.tie(nullptr)
 //stol(s);sqrtl()     to_string(x);
-const ld PI = acos((ld) - 1);
-const ll INF = 2e18 + 1;
-const ll MOD = 1e9 + 7;
-const ld EPS = 1e-9;
-const int MX = 2e6;
-int  cs = 1;
 
 #ifdef LOCAL
 #include "debug.h"
@@ -32,70 +19,33 @@ int  cs = 1;
 #define debug(...)
 #endif
 
-
-/*struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};*/
-
-
 void solve() {
-    int n, x;   cin >> n >> x;
-    int n1 = n / 2, n2 = n - (n / 2);
+    int n;  cin >> n;
+    string a, b;   cin >> a >> b;
 
-    vector <int> v1(n1) , v2(n2);
-    ll sum = 0, ans = 0;
-
-    for (int i = 0; i < n1; i++) cin >> v1[i];
-    for (int i = 0; i < n2; i++) cin >> v2[i];
-
-    gp_hash_table<ll, int> mp;
-
-    for (int mask = 0; mask < (1 << n1); mask++) {
-        for (int i = 0; i < n1; i++) {
-            if (mask & (1 << i)) sum += v1[i];
+    for (int i = 0; i < n; i++) {
+        int cnt = 0;
+        while (a[i] != b[i]) {
+            cnt++;
+            if (a[i] - 'A' + 3 >= 26) {
+                // cerr << a[i] << ' ' << signed(a[i] - 'A') + 3 << endl;
+                a[i] = 'A' + (a[i] - 'A' + 3) % 26;
+            }
+            else a[i] += 3;
+            // debug(a, b);
         }
-        mp[sum]++;
-        sum = 0;
-    }
-
-    for (int mask = 0; mask < (1 << n2); mask++) {
-        for (int i = 0; i < n2; i++) {
-            if (mask & (1 << i))  sum += v2[i];
-        }
-        if (mp.find(x - sum) != mp.end())  ans += mp[x - sum];
-        sum = 0;
-    }
-
-    cout << ans << endl;
+        cout << cnt << ' ';
+    } cout << endl;
 }
 
-int main() {
-
-#ifdef LOCAL
-    clock_t tStart = clock();
-#endif
-
-    unsyncIO;
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int t = 1;
-    //cin >> t;
+    cin >> t;
     //cin.ignore();
     while (t--) {
         solve();
     }
-
-#ifdef LOCAL
-    cerr << prec(5) << "\nRuntime: " << (ld) (clock() - tStart) / CLOCKS_PER_SEC << " Seconds" << endl;
-#endif
     return 0;
 }
--- temp.cpp
+++ temp.cpp
@@ -1,86 +1,53 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
 using ld = long double;
+const ld PI = acos((ld) - 1);
 using ull = unsigned long long;
 
 #define endl          '\n'
-#define gcd(x,y)      __gcd(x,y)
+#define int 	      long long
 #define sz(x)         (int)x.size()
-#define lcm(x,y)      ((x/gcd(x,y)) * y)
 #define mem(a,x)      memset(a,x,sizeof(a))
 #define all(x)        (x).begin(),(x).end()
 #define rall(x)       (x).rbegin(),(x).rend()
-#define prec(x)       fixed << setprecision(x)
-#define testcase      cout << "Case " << cs++ << ":"
-#define unsyncIO      ios_base::sync_with_stdio(false); cin.tie(nullptr)
 //stol(s);sqrtl()     to_string(x);
-const ld PI = acos((ld) - 1);
-const ll INF = 2e18 + 1;
-const ll MOD = 1e9 + 7;
-const ld EPS = 1e-9;
-const int MX = 2e6;
-int  cs = 1;
 
 #ifdef LOCAL
 #include "debug.h"
 #else
 #define debug(...)
 #endif
+const int N = 2e5 + 5;
+int n, a[N];
 
-void solve() {
-	ll n; cin >> n;
-	vector <int> primes;
-	int len = sqrtl(n) + 1;
-	for (int i = 2; i <= len; i++) {
-		if (n % i == 0) {
-			primes.push_back(i);
-			while (n % i == 0) n /= i;
-		}
+int l = 1, r = n, sl = 1, sr = n;
+void func() {
+	l = 1, r = n, sl = 1, sr = n;
+	int x = a[l];
+	while (l <= n and a[l] == x) l++;
+	x = a[r];
+	while (r >= 1 and a[r] == x) r--;
+	while (sl < sr and a[sl] == a[sr]) {
+		sr--, sl++;
 	}
-	vector <bool> vis(n, 1);
-	for (auto it : primes) {
-		for (int i = it; i < n; i += it) vis[i] = 0;
-	}
-
-	ll prod = 1;
-	vector <int> store;
-	store.push_back(1);
-
-	for (int i = 2; i < n; i++) {
-		if ((prod * i) < n and vis[i]) {
-			store.push_back(i);
-			prod *= i;
-		}
-		else if (vis[i] and (prod * i) % n == 1) {
-			prod *= i;
-			store.push_back(i);
-		}
-	}
-
-	cout << sz(store) << endl;
-	for (auto it : store) cout << it << ' ';
-	cout << endl;
-
 }
-
-int main() {
-
-#ifdef LOCAL
-	clock_t tStart = clock();
-#endif
-
-	unsyncIO;
+void solve() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> a[i];
+	func();
+	int ans1 = (n - l + 1);
+	int ans2 = (r - 1 + 1);
+	cout << min({ans1, ans2, (sr - sl + 1)}) << '\n';
+}
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 	int t = 1;
-	//cin >> t;
+	cin >> t;
 	//cin.ignore();
 	while (t--) {
 		solve();
 	}
-
-#ifdef LOCAL
-	cerr << prec(5) << "\nRuntime: " << (ld) (clock() - tStart) / CLOCKS_PER_SEC << " Seconds" << endl;
-#endif
 	return 0;
 }
