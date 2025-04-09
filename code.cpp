f83f981988163b1271559276520bc94f3bd31eb4
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2025-04-10 00:37:20 +0600

    init

--- code.cpp
+++ code.cpp
@@ -7,7 +7,6 @@ using ull = unsigned long long;
 #define endl          '\n'
 #define int           long long
 #define sz(x)         (int)x.size()
-#define mem(a,x)      memset(a,x,sizeof(a))
 #define all(x)        (x).begin(),(x).end()
 #define rall(x)       (x).rbegin(),(x).rend()
 //to_string(x)             sqrtl()   stol(s);
@@ -22,7 +21,42 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 #endif
 
 void solve() {
-   cout << 1 << endl;
+   int n, m, x;
+   cin >> n >> m >> x;
+   vector <pair<int, int>> v;
+   vector <int> indx(n+1);
+
+   for(int i = 0; i < n; i++){
+      int val; cin >> val;
+      v.push_back({val, i});
+   }
+
+   sort(rall(v));
+
+   set <pair<int, int>> st;
+
+   for(int i = 0; i < m; i++){
+      st.insert({v[i].first, i});
+      indx[v[i].second] = i;
+   }
+
+   for(int i = m; i < n; i++){
+      int val = st.begin()->first;
+      int idx = st.begin()->second;
+
+      st.erase(st.begin());
+      st.insert({val + v[i].first, idx});
+      indx[v[i].second] = idx;
+   }
+
+   int first = st.begin()->first, last = st.rbegin()->first;
+   
+   if(last - first <= x){
+      cout << "YES" << endl;
+      for(int i = 0; i < n; i++) cout << indx[i] + 1 << ' ';
+      cout << endl;
+   }
+   else cout << "NO" << endl;
 }
 
 signed main() {
