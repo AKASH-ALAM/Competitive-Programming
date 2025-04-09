45879cdeee414ee39bee5d12beab9652976571c0
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2025-04-10 00:38:36 +0600

    update

--- cf_template.cpp
+++ cf_template.cpp
@@ -9,14 +9,12 @@ using ull = unsigned long long;
 #define endl          '\n'
 #define int           long long
 #define sz(x)         (int)x.size()
-#define mem(a,x)      memset(a,x,sizeof(a))
 #define all(x)        (x).begin(),(x).end()
 #define rall(x)       (x).rbegin(),(x).rend()
 //to_string(x)             sqrtl()   stol(s);
 
 template              <typename T>
 using minHeap = priority_queue<T, vector<T>, greater<T>>;
-
 
 #ifdef LOCAL
 #include "debug.h"
@@ -32,7 +30,7 @@ signed main() {
    ios_base::sync_with_stdio(false);
    cin.tie(nullptr);
    int t = 1;
-   // cin >> t;
+   cin >> t;
    //cin.ignore();
    while (t--) {
       solve();
